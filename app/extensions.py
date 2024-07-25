2fe02ef962d969994e09014af6280fbcff518a5b
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2024-07-25 12:49:11 +0800

    chore: Add Flask-Login and Flask-CORS extensions, update JWT configuration, and add LoginManager

--- app/extensions.py
+++ app/extensions.py
@@ -1,10 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
+from flask_login import LoginManager
 
 
 db = SQLAlchemy()
 migrate = Migrate()
 cors = CORS()
 jwt_manager = JWTManager()
+login_manager = LoginManager()
