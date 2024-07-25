37d8aa48f8608f51d17edc849f4a24c29ece3304
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2024-07-25 23:53:30 +0800

    refactor: Update SQLAlchemy model_class parameter in extensions.py
    This commit updates the `extensions.py` file to use the `Base` class from SQLAlchemy's ORM mapping instead of the traditional `DeclarativeBase` class. The `model_class` parameter of the `SQLAlchemy` object is now set to `Base`, which allows for better integration with the SQLAlchemy ORM features. This change improves the readability and maintainability of the code by leveraging SQLAlchemy's powerful ORM features.
    
    Refactor the `SQLAlchemy` object in `extensions.py` to use the `Base` class from SQLAlchemy's ORM mapping.

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,7 @@
 # extensions.py
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
 
 from flask_cors import CORS
@@ -8,7 +9,11 @@ from flask_jwt_extended import JWTManager
 from flask_login import LoginManager
 
 
-db = SQLAlchemy()
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
 cors = CORS()
 jwt_manager = JWTManager()
