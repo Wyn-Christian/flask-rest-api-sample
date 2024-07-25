d180bf54c33656b017f03a5d174a38b84dece377
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2024-07-25 12:31:25 +0800

    chore: Update JWT configuration for cookie-based authentication
    This commit updates the JWT configuration in the `config.py` file. The `JWT_COOKIE_SECURE` parameter is set to `False` to allow the JWT access token to be transmitted over non-secure connections. The `JWT_TOKEN_LOCATION` parameter is updated to include both "cookies" and "headers" to support authentication via cookies and headers. The `JWT_SECRET_KEY` parameter is set to a default value of "super-secret" if no environment variable is provided. The `JWT_ACCESS_TOKEN_EXPIRES` parameter is set to expire after 8 hours. The `JWT_COOKIE_NAME` and `JWT_REFRESH_TOKEN_COOKIE_NAME` parameters are set to custom cookie names. Finally, the `JWT_COOKIE_CSRF_PROTECT` parameter is set to `False` to disable CSRF protection for JWT cookies.
    
    This commit follows the established convention of using the "chore" type for non-functional changes and provides a clear and concise description of the modifications made.

--- app/config.py
+++ app/config.py
@@ -1,7 +1,17 @@
 import os
+from datetime import timedelta
 
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess-eme"
+
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///app.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    JWT_COOKIE_SECURE = False
+    JWT_TOKEN_LOCATION = ["cookies", "headers"]
+    JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "super-secret"
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=8)
+    JWT_COOKIE_NAME = "access_token_cookie"
+    JWT_REFRESH_TOKEN_COOKIE_NAME = "refresh_token_cookie"
+    JWT_COOKIE_CSRF_PROTECT = False
