856b542f2ef9ee470637dfdaba4c633c3f0df3c4
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2024-07-25 12:49:19 +0800

    chore: Add Flask-Login and Flask-CORS extensions, update JWT configuration, and add LoginManager

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 
-from app.extensions import db, migrate, cors, jwt_manager
+from app.extensions import db, migrate, cors, jwt_manager, login_manager
 from app.routes import initialize_routes
 
 
@@ -16,6 +16,7 @@ def create_app():
     migrate.init_app(app, db)
     cors.init_app(app)
     jwt_manager.init_app(app)
+    login_manager.init_app(app)
 
     api = Api(app, catch_all_404s=True)
     initialize_routes(api)
