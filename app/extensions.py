4dabc5e8cf06ad3fb082eb6173001536ec9e9113
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2024-07-26 16:08:54 +0800

    refactor: Update extensions.py to include Flask-Mail
    This commit updates the `extensions.py` file to import and initialize the `Mail` extension from Flask-Mail. The `Mail` extension is used for sending emails in the application. By including this extension, we can now easily send emails using Flask-Mail, improving the functionality and user experience of the application.
    
    Update `extensions.py` to include the `Mail` extension from Flask-Mail.

--- app/extensions.py
+++ app/extensions.py
@@ -4,9 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
 
+from flask_mail import Mail
+
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-from flask_login import LoginManager
 
 
 class Base(DeclarativeBase):
@@ -15,6 +16,6 @@ class Base(DeclarativeBase):
 
 db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
+mail = Mail()
 cors = CORS()
 jwt_manager = JWTManager()
-login_manager = LoginManager()
