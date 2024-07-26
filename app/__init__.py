67ad24acfbc8a9675f5fe161556bdd17a2895d18
Wyn Chrsitian <80365421+Wyn-Christian@users.noreply.github.com>
2024-07-26 16:09:00 +0800

    feat: Add Flask-Mail extension for sending emails

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 # __init__.py
 
+import logging
 from flask import Flask
 from flask_restful import Api
 
-from app.extensions import db, migrate, cors, jwt_manager, login_manager
+from app.extensions import db, migrate, mail, cors, jwt_manager
 from app.routes import initialize_routes
 from app.utils.errors import register_error_handlers
 
@@ -17,9 +18,9 @@ def create_app():
         db.create_all()
 
     migrate.init_app(app, db)
+    mail.init_app(app)
     cors.init_app(app)
     jwt_manager.init_app(app)
-    login_manager.init_app(app)
 
     api = Api(app)
     initialize_routes(api)
